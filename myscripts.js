f5ba8d48a7d77e1339c136432a78d0a2dd6d4bcd
amrita41 <amritakumari9898@gmail.com>
2020-06-02 19:23:49 +0530

    Initial Commit

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function()
+{
+  $('.post-wrapper').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    nextArrow: $('next'),
+    prevArrow: $('prev')
+
+});
+  
+});
